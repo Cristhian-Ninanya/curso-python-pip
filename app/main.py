54bd7d7a09f23b1c258f619ae0e9a2d9cf14225e
Cristhian Ninanya <cristhianninanya@gmail.com>
2023-01-09 21:21:42 -0500

    Update app

--- app/main.py
+++ app/main.py
@@ -1,19 +1,30 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 #Algoritmo para graficar la poblacion por pais y mundial:
 def run():
+    #Sin utilizar libreria pandas:
+    '''
     data = read_csv.read_csv('data.csv')
     data = list(filter(lambda item:item['Continent'] == 'South America',data))
     
     countries = list(map(lambda x:x['Country/Territory'],data))
     percentages = list(map(lambda x:x['World Population Percentage'],data))
     charts.generate_pie_chart(countries,percentages)
+    '''
+    #Utilizando libreria pandas
+    df = pd.read_csv('data.csv')
+    df = df[df['Continent']=='Africa']
+    countries = df['Country/Territory'].values
+    percentages = df['World Population Percentage'].values
+    charts.generate_pie_chart(countries,percentages)
 
+    #Codigo para ingresar pais especifico
+    data = read_csv.read_csv('data.csv')
     country = input('Type Country :') #Ingresar nombre de pais:
     print('Pais elegido:',country)
-
     result = utils.population_by_country(data,country)
 
     if len(result) > 0:
